75053adc7d567cf1396772fca9a78fdd42a6f706
flizotte <flizotte@decclic.qc.ca>
2016-11-16 17:28:57 -0500

    Update mod_form.php

--- mod_form.php
+++ mod_form.php
@@ -58,7 +58,7 @@ class mod_game_mod_form extends moodleform_mod {
 
         $mform->addElement('header', 'general', get_string('general', 'form'));
 
-        $mform->addElement('text', 'name', 'Name', array('size' => '64'));
+        $mform->addElement('text', 'name', get_string('name', 'game'), array('size' => '64'));
         if (!empty($CFG->formatstringstriptags)) {
             $mform->setType('name', PARAM_TEXT);
         } else {
