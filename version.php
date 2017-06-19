ad41b211ebd95774b45708994f377b6e45113a10
Ebben Feagan <ebben@tpv.com>
2017-06-19 12:49:38 -0500

    Changed rows/cols in snake_database table for compatability with MariaDB

--- version.php
+++ version.php
@@ -24,23 +24,23 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-if (!isset( $plugin)) {
-    $plugin = new stdClass;
-    $useplugin = 0;
+if (!isset($plugin)) {
+	$plugin = new stdClass;
+	$useplugin = 0;
 } else if ($plugin == 'mod_game') {
-    $plugin = new stdClass;
-    $useplugin = 1;
+	$plugin = new stdClass;
+	$useplugin = 1;
 } else {
-    $useplugin = 2;
+	$useplugin = 2;
 }
 
-$plugin->component = 'mod_game';  // Full name of the plugin (used for diagnostics).
-$plugin->version   = 2017061901;  // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2010112400;  // Requires Moodle 2.0.
-$plugin->cron      = 0;           // Period for cron to check this module (secs).
-$plugin->release   = '2017-06-19';
+$plugin->component = 'mod_game'; // Full name of the plugin (used for diagnostics).
+$plugin->version = 2017061902; // The current module version (Date: YYYYMMDDXX).
+$plugin->requires = 2010112400; // Requires Moodle 2.0.
+$plugin->cron = 0; // Period for cron to check this module (secs).
+$plugin->release = '2017-06-19';
 $plugin->maturity = MATURITY_STABLE;
 
 if ($useplugin != 2) {
-    $module = $plugin;
+	$module = $plugin;
 }
