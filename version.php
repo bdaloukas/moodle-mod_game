1359a813207fb6bd8563551bfa856946d2cc4a61
Vasilis Daloukas <bdaloukas@gmail.com>
2017-06-27 10:58:03 +0300

    Fix MariaDB compatibility

--- version.php
+++ version.php
@@ -38,7 +38,7 @@ $plugin->component = 'mod_game';  // Full name of the plugin (used for diagnosti
 $plugin->version   = 2017062702;  // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2010112400;  // Requires Moodle 2.0.
 $plugin->cron      = 0;           // Period for cron to check this module (secs).
-$plugin->release   = '2017-06-20';
+$plugin->release   = '2017-06-27';
 $plugin->maturity = MATURITY_STABLE;
 
 if ($useplugin != 2) {
